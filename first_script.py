7cd94b36bf0db5a51fb2277e4472af4b517c43d0
AshenVoid <richardegydy@gmail.com>
2024-12-04 18:47:49 +0100

    První description HYPE

--- first_script.py
+++ first_script.py
@@ -1,1 +1,28 @@
 print("WADDUP PARTY PEOPLE")
+
+part1 = "Hello"
+part2 = "World"
+part3 = "!"
+space = " "
+sentence = []
+
+#Resolve if part1 == specified string
+if part1 == "Hello":
+    print("First correct")
+else:
+    part1 = []
+
+#Resolve if part2 == specified string
+if part2 == "World":
+    print("Second correct")
+else:
+    part2 = []
+
+#Resolve if part3 == specified string
+if part3 == "!":
+    print("Third correct")
+else:
+    part3 = []
+
+sentence = f"{part1}{space}{part2}{part3}"
+print(sentence)
