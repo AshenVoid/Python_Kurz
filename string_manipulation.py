30431ea60432792baf212b963635996de7f67d14
AshenVoid <richardegydy@gmail.com>
2024-12-04 20:03:47 +0100

    1.0.0b - Small formatting changes

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,6 +1,6 @@
 
-#len() looks for the number of characters starting with 0
-#index() looks for specified character and returns its index value
+#Function len() looks for the number of characters starting with 0
+#Function index() looks for specified character and returns its index value
 str = input("Insert any sentence to play with it: ")
-print(f'str.index(), the thing you were looking for is indexed at:  {str.index(input("What do you want to look for? "))}')
+print(f'str.index(), the thing you were looking for is indexed at: {str.index(input("What do you want to look for? "))}')
 print(f"len(str), the length of this sentence (string) is: {len(str)}")
