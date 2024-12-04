1c83118ef1c6f7c45bd9fa45c5654b6c654674a5
AshenVoid <richardegydy@gmail.com>
2024-12-04 19:23:29 +0100

    1.0.1b - Expanded input features and added a % human tracker

--- decimal_formatting.py
+++ decimal_formatting.py
@@ -2,9 +2,16 @@
 name = input("Name: ")
 surname = input("Surname: ")
 age = input("Age: ")
+number_of_limbs = input("How many limbs do you have: ")
 year_variable = "years"
+
 
 if age == "1":
     year_variable = "year"
 
-print(f"I see, your name is {name} {surname} and you are {age} {year_variable} old, now I know everything")
+
+print(f"I see, your name is {name} {surname} and you are {age} {year_variable} old, I can also see that you have around {number_of_limbs} limbs, now I know everything")
+
+percent_of_human = (int(number_of_limbs) / 4) * 100
+
+print(f"That makes you {percent_of_human}% human, good job!")
