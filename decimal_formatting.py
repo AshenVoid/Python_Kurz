4dd644a5db45955f44eacda46c07ca595d363978
AshenVoid <richardegydy@gmail.com>
2024-12-04 19:46:00 +0100

    1.0.1c - More expansion, MORE CHANGES

--- decimal_formatting.py
+++ decimal_formatting.py
@@ -4,14 +4,20 @@ surname = input("Surname: ")
 age = input("Age: ")
 number_of_limbs = input("How many limbs do you have: ")
 year_variable = "years"
-
+good_or_bad = "% human, good job!"
 
 if age == "1":
     year_variable = "year"
 
 
-print(f"I see, your name is {name} {surname} and you are {age} {year_variable} old, I can also see that you have around {number_of_limbs} limbs, now I know everything")
+print(f"I see, your name is {name} {surname} and you are {age} {year_variable} old, I can also see that you have around {number_of_limbs} limbs, now I know everything.")
 
 percent_of_human = (int(number_of_limbs) / 4) * 100
 
-print(f"That makes you {percent_of_human}% human, good job!")
+if percent_of_human > 100:
+    percent_of_human = "a goddamn alien"
+
+if percent_of_human == "a goddamn alien":
+    good_or_bad = ", the authorities were notified... RUN."
+
+print(f"That makes you {percent_of_human}{good_or_bad}")
