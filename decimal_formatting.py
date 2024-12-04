72c1ceb4ff94307ba48a2af243606ad6b346c562
AshenVoid <richardegydy@gmail.com>
2024-12-04 19:12:44 +0100

    1.0.1a - Small changes to correct formating of years

--- decimal_formatting.py
+++ decimal_formatting.py
@@ -2,4 +2,9 @@
 name = input("Name: ")
 surname = input("Surname: ")
 age = input("Age: ")
-print(f"I see, your name is {name} {surname} and you are {age} years old, now I know everything")
+year_variable = "years"
+
+if age == "1":
+    year_variable = "year"
+
+print(f"I see, your name is {name} {surname} and you are {age} {year_variable} old, now I know everything")
