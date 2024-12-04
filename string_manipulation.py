d44204b032e06fd1b71d63061f9e4d057d98bc3d
AshenVoid <richardegydy@gmail.com>
2024-12-04 19:59:27 +0100

    1.0.0 - String manipulation functions len() and index()

---
+++ string_manipulation.py
@@ -0,0 +1,4 @@
+
+str = input("Insert any sentence to play with it: ")
+print(f'str.index(), the thing you were looking for is indexed at:  {str.index(input("What do you want to look for? "))}')
+print(f"len(str), the length of this sentence (string) is: {len(str)}")
