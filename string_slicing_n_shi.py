83def832f7185392b30cd8be18a1347fc10815b6
AshenVoid <richardegydy@gmail.com>
2024-12-06 17:59:01 +0100

    1.0.0 - String slicing and manipulation

---
+++ string_slicing_n_shi.py
@@ -0,0 +1,9 @@
+text = "Pomocí speciálního příkazu s operátorem [] můžeme řetězec přečíst od konce?"
+
+
+#Methodics of string slicing and manipulation
+print(f"V textu se znak 's' nachází {text.count("s")}")
+print(f"Zároveň zde můžeme vidět tuto část na indexu 7 - 14: {text[7:14]}")
+print(f"Při této metodice můžeme vyříznout slovo, třeba toto: {text[29:39]}")
+print(f"Můžeme vypsat i každý druhý znak: {text[1:76:2]}")
+print(f"Či celý text obrátit: {text[::-1]}")
