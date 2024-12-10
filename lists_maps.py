18f6a6bfc68d08ee01288d360d28884436d322b1
AshenVoid <richardegydy@gmail.com>
2024-12-10 19:16:42 +0100

    Practical Exercise

---
+++ lists_maps.py
@@ -0,0 +1,11 @@
+collection_list = [["Lenka", ["Alice",["Kleopatra"]]]]
+
+#collection_list.append(input("První věc co chcete přidat do listu: "))
+#collection_list.append(input("Druhou věc co chcete přidat do listu: "))
+#collection_list.append(input("Třetí věc co chcete přidat do listu: "))
+#print(f"Celkově jste přidali tři věci a ty jsou: {collection_list}")
+#print(f"Věc na prvním indexu je {collection_list[0]}")
+#print(f"Věc na druhém indexu je {collection_list[1]}")
+#print(f"Věc na třetím indexu je {collection_list[2]}")
+
+print(collection_list[0][1][0])
